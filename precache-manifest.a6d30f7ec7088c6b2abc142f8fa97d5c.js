928604a22fd8136ce54dfb80f9f4834ebddfd000
jenwin <nguy.jenni@gmail.com>
2021-05-27 16:05:44 -0500

    Updates

---
+++ precache-manifest.a6d30f7ec7088c6b2abc142f8fa97d5c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5eceacc4912ac5262e6d",
+    "url": "/myreads-app/static/css/main.57778a11.chunk.css"
+  },
+  {
+    "revision": "5eceacc4912ac5262e6d",
+    "url": "/myreads-app/static/js/main.5eceacc4.chunk.js"
+  },
+  {
+    "revision": "52d0b1b390ce3e1a3e85",
+    "url": "/myreads-app/static/js/1.52d0b1b3.chunk.js"
+  },
+  {
+    "revision": "1500c43aa4a67be84717",
+    "url": "/myreads-app/static/js/runtime~main.1500c43a.js"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/myreads-app/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/myreads-app/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/myreads-app/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "a9a6670a3b2134d4fc9292ca0d8715d0",
+    "url": "/myreads-app/index.html"
+  }
+];
